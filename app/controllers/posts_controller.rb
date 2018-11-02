4484094889c8af17f29c7c6788a8d93a19b95f99
yli0607x <github email address>
2018-11-02 13:11:14 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,40 @@
 class PostsController < ApplicationController
-	def index
-		@posts = Post.all
-	end
+    def index
+        @posts = Post.all
+    end
 
-	def show
-		@post = Post.find(params[:id])
-	end
+    def show
+        @post = Post.find(params[:id])
+    end
 
-	def new
-		@post = Post.new
-	end
+    def new
+        @post = Post.new
+    end
 
-	def create
-	  @post = Post.new(params["post"])
-	  @post.save
-	  redirect_to post_path(@post)
-	end
+    def create
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to post_path(@post)
+        # @post = Post.new(params["post"])
+        # @post.save
+        # redirect_to post_path(@post)
+    end
 
-	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
-	end
+    def update
+			@post = Post.find(params[:id])
+      @post.update(post_params)
+      redirect_to post_path(@post)
 
-	def edit
-	  @post = Post.find(params[:id])
-	end
+        # @post = Post.find(params[:id])
+        # @post.update(params['post'])
+        # redirect_to post_path(@post)
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+		def post_params
+			params.require(:post).permit(:title, :description)
+		end
 end
